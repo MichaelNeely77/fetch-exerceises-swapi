43a73f140836b8e894e5fdbf45bf92319ed70600
Michael Neely <michael@michaelneely.net>
2020-06-29 23:49:47 -0400

    Added the elements to index.htnml and adding bootstrap with JS too

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const url = 'https://swapi.dev/api/planets';
 
-function fetchData() {
+const button = document.createElement('button');
+
+
+button.textContent = 'Press me';
+document.body.appendChild(button);
+button.setAttribute('class', 'btn btn-primary')
+document.querySelector('.container').appendChild(button);
+
+button.addEventListener('click', function() {
+    fetchData('https://swapi.dev/api/planets');
+});
+const output = document.createElement('div');
+document.body.appendChild(output);
+document.querySelector('.container').appendChild(output);
+
+function fetchData(url) {
     fetch(url).then(function(res) {
         return res.json()
     }).then(function(data){
+        output.textContent = `${data.count} result found`;
+        if(data.next){
+            const buttonNext = document.createElement('button');
+            buttonNext.setAttribute('class', 'btn btn-primary m-2');
+            buttonNext.textContent = 'Next';
+            output.appendChild(buttonNext);
+            buttonNext.addEventListener('click', function(){
+                fetchData(data.next);
+            });
+        }
         console.log(data);
     })
 }
